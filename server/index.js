2feb2e41acaf5f95f509550839dbaa105a918b71
veranika-karpava <veranika.karpava@gmail.com>
2022-06-16 16:55:39 -0700

    Built server-side folder & server

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const fs = require('fs'); //for working with file system
+const PORT = 8080 || 5050;
+
+
+// function for reading data in data.json
+const readFile = () => {
+    const toDoData = fs.readFileSync('./data/data.json');
+    return JSON.parse(toDoData);
+}
+
+// function for writing data in data.json
+const writeFile = (toDoData) => {
+    fs.writeFileSync('./data/data.json', JSON.stringify(toDoData, null, 2));
+}
+
+// get toDo name from data.json file
+app.get('/', (_req, res) => {
+    let toDoData = readFile();
+    toDoData = toDoData.map(task => {
+        return {
+            id: task.id,
+            name: task.name,
+            completed: task.completed
+        }
+    })
+    res.status(200).send(toDoData)
+})
+
+
+// port 
+app.listen(PORT, () => {
+    console.log(`🚀 Server listening on ${PORT}`)
+})
