bc6fd4a13097d442a34af1b43a559bab3c8466ef
veranika-karpava <veranika.karpava@gmail.com>
2022-06-16 17:20:50 -0700

    Server side changes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const fs = require('fs'); //for working with file system
 const PORT = 8080 || 5050;
 
+//middleware
+//CORS
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
 
 // function for reading data in data.json
 const readFile = () => {
