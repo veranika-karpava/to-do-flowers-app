8b965c93176fbe702146188bafc5d70c700156a9
veranika-karpava <veranika.karpava@gmail.com>
2022-07-21 13:22:58 -0700

    Added github icon to footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Footer.scss';
+import { AiFillGithub } from "react-icons/ai";
 
 const Footer = ({ isDark }) => {
     return (
         <footer className={!isDark ? 'footer' : 'footer footer__dark'}>
-            <p className='footer__heading'>Created by <a href="https://www.vkarpava.tech" className='footer__link'>Veranika Karpava</a></p>
+            <div className='footer__container-content'>
+                <p className='footer__content'>Created by <a href="https://www.vkarpava.tech" className='footer__link'>Veranika Karpava</a></p>
+            </div>
+            <div className='footer__container-link'>
+                <a className='footer__link-github' href='https://github.com/veranika-karpava/to-do-app-gardens' target='blank'>
+                    <AiFillGithub className='footer__icon' />
+                </a>
+            </div>
         </footer>
     );
 };
