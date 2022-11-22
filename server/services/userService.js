255dd1ca470eb0ede5464f77eba2228097fb36a4
veranika-karpava <veranika.karpava@gmail.com>
2022-11-21 18:56:20 -0800

    Add login function for loging existing user

--- server/services/userService.js
+++ server/services/userService.js
@@ -76,6 +76,50 @@ const logIn = async (req, res, next) => {
         return next(new HttpError('Please make sure to include all inputs.', 422));
     }
 
+    // check existing user with specified username
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ username: username });
+    } catch (err) {
+        return next(new HttpError('Logging in failed, please try again later.', 500));
+    };
+
+    if (!existingUser) {
+        return next(new HttpError('User is not found, please check your username', 422));
+    };
+
+    // check password
+    let isValidPassword;
+    try {
+        isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch (err) {
+        return next(new HttpError('Could not log you in, please try again.', 500));
+    };
+
+    if (!isValidPassword) {
+        return next(new HttpError('Please check your password, could not log you in.', 403));
+    }
+
+    // generate jwt token
+    let jwtToken;
+    try {
+        jwtToken = jwt.sign(
+            { userId: existingUser.id, username: existingUser.username },
+            JWT_KEY,
+            { expiresIn: '1h' }
+        );
+    } catch (err) {
+        return next(new HttpError('Logging in failed, please try again', 500));
+    }
+
+    res.status(200).json({ userId: existingUser.id, username: existingUser.username, jwtToken: jwtToken });
+
+
+
+
+
+
+
 
 };
 
