a44a4af6002c6acd2ca2d86ae642127c5a60a662
veranika-karpava <veranika.karpava@gmail.com>
2022-11-28 22:54:55 -0800

    Update model for task

--- server/models/task.js
+++ server/models/task.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 const taskSchema = new Schema({
     title: { type: String, required: true },
     completed: { type: Boolean, default: false },
-    creator: { type: mongoose.Types.ObjectId, require: true, ref: 'User' }
 });
 
 module.exports = mongoose.model('Task', taskSchema);
