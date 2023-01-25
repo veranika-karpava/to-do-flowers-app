4d19eebfba53ad3dcf91867aee15dee034ed6358
veranika-karpava <veranika.karpava@gmail.com>
2023-01-25 15:12:32 -0800

    Built DynamicIcon component

---
+++ client/src/components/DynamicIcon/DynamicIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { icons } from '../../data/icons.data';
+
+const DynamicIcon = ({ name, className }) => {
+    const DynamicIcon = icons[name];
+    return <DynamicIcon className={className} />;
+};
+
+export default DynamicIcon;
---
+++ client/src/data/icons.data.js
@@ -0,0 +1,13 @@
+import { FaHeart } from 'react-icons/fa';
+import { BsMoon, BsSun } from 'react-icons/bs';
+import { BiUser, BiExit } from 'react-icons/bi';
+import { AiFillGithub } from "react-icons/ai";
+
+export const icons = {
+  FaHeart: FaHeart,
+  BsMoon: BsMoon,
+  BsSun: BsSun,
+  BiUser: BiUser,
+  BiExit: BiExit,
+  AiFillGithub: AiFillGithub
+};
