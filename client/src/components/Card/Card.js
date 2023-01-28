7ba4ce0cc2d506bb6f396f7c65f8be01b93e3ac1
veranika-karpava <veranika.karpava@gmail.com>
2023-01-27 23:17:46 -0800

    Build general layot for components

---
+++ client/src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+
+import { ThemeContext } from '../../helpers/context/ThemeContext';
+import './Card.scss';
+
+const Card = ({ children }) => {
+    const theme = useContext(ThemeContext);
+
+    return (
+        <div className={`card card__${theme.theme}`}>
+            {children}
+        </div>
+    );
+};
+
+export default Card;
