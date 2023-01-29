d13c36ec55dbfa122f1d17e3aa86ae22fa15cf4c
veranika-karpava <veranika.karpava@gmail.com>
2023-01-28 22:55:15 -0800

    Create component for error message

---
+++ client/src/components/ErrorMessage/ErrorMessage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './ErrorMessage.scss';
+
+const ErrorMessage = ({ errorText, textAlign }) => {
+
+    return (
+        <div className='error-message' style={{ textAlign: textAlign }}>
+            <p className='error__message__content'>{errorText}</p>
+        </div>
+    );
+};
+
+export default ErrorMessage;
