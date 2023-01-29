78627767405e73d54b1413b16e3adcc55f71dd2e
veranika-karpava <veranika.karpava@gmail.com>
2023-01-28 23:02:49 -0800

    Add logout handler to exit icon

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,19 +2,23 @@ import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import DynamicIcon from '../DynamicIcon/DynamicIcon';
-import './Header.scss';
 import { ThemeContext } from '../../helpers/context/ThemeContext';
+import { AuthContext } from '../../helpers/context/AuthContext';
+import './Header.scss';
+
 
 const Header = () => {
     const theme = useContext(ThemeContext);
+    const auth = useContext(AuthContext);
+    console.log(auth)
 
     return (
         <header className={`header header__${theme.theme}`}>
             <div className='header__container'>
                 <div className='header__nav-item'>
-                    <NavLink className='header__nav-link' exact to='/'>
+                    {auth.isLoggedIn && (<NavLink className='header__nav-link' exact to='/' onClick={auth.logout}>
                         <DynamicIcon name='BiExit' className='header__icon' />
-                    </NavLink>
+                    </NavLink>)}
                 </div>
                 <div className='header__wrapper'>
                     <h1 className='header__heading'>todo</h1>
