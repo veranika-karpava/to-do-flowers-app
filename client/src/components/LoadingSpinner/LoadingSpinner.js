4a6ce1230c169a60e212367033daae66a62a7a12
veranika-karpava <veranika.karpava@gmail.com>
2023-01-30 12:42:32 -0800

    Built loading spinner and style it

---
+++ client/src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './LoadingSpinner.scss';
+import DynamicIcon from '../DynamicIcon/DynamicIcon';
+
+const LoadingSpinner = () => {
+    return (
+        <div className='loading'>
+            <DynamicIcon name='VscLoading' className='loading__icon' />
+        </div>
+    );
+};
+
+export default LoadingSpinner;
