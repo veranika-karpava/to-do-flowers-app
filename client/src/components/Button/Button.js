0f628a22a8bd62bc15f19e2df30105324c435e74
veranika-karpava <veranika.karpava@gmail.com>
2023-01-30 16:43:35 -0800

    Add new button for deleting task

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import './Button.scss';
+import React, { useContext } from 'react';
 
-const Button = ({ title, isDark, onClick, value }) => {
+import './Button.scss';
+import { ThemeContext } from '../../helpers/context/ThemeContext';
+
+const Button = ({ shape, type, title, onClick, children, disabled }) => {
+    const theme = useContext(ThemeContext);
+
     return (
-        <button type='button' className={!isDark ? 'button' : 'button button__dark'} onClick={onClick} value={value}>{title}</button>
+        <button
+            type={type}
+            className={`button button__${shape} button__${shape}--${theme.theme}`}
+            onClick={onClick}
+            disabled={disabled}>
+            {title}
+            {children}
+            {disabled}
+        </button>
     );
 };
 
