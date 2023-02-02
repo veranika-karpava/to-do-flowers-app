c560383e549aa760570d103ca10fbb44bf45fa53
veranika-karpava <veranika.karpava@gmail.com>
2023-02-01 21:52:15 -0800

    Add style for nav button and clean up style

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -3,15 +3,17 @@ import React, { useContext } from 'react';
 import './Button.scss';
 import { ThemeContext } from '../../helpers/context/ThemeContext';
 
-const Button = ({ shape, type, title, onClick, children, disabled }) => {
+const Button = ({ shape, type, title, onClick, children, disabled, value, active }) => {
     const theme = useContext(ThemeContext);
 
     return (
         <button
             type={type}
-            className={`button button__${shape} button__${shape}--${theme.theme}`}
+            className={`button button__${shape} ${active && `button__${shape}--${active}`} button__${shape}--${theme.theme}`}
             onClick={onClick}
-            disabled={disabled}>
+            disabled={disabled}
+            value={value}
+        >
             {title}
             {children}
             {disabled}
